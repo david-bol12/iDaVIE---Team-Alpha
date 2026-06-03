8b38f9d1bcdad63d22737bbd35342bd373f6c724
Mhe115 <conor.healy490@gmail.com>
2026-06-03 13:57:05 +0100

    Revert "Restore sub-team-6 and handoffs files accidentally removed from branch"
    This reverts commit 8438f43c3c5b1fc613f4f2b2fc1042a969f99de1.

--- refactoring-examples/sub-team-6/debug-tab/skeleton/ILogObserver.cs
+++
@@ -1,8 +0,0 @@
-namespace iDaVIE.Desktop.DebugTab
-{
-    public interface ILogObserver
-    {
-        //Called by the stream on whatever thread Publish was invoked from.
-        void OnNext(LogEntry entry);
-    }
-}
